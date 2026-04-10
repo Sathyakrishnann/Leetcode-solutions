27680be299945f30c988cac7d3a949878002fd2c
Sathya Kala <sathyakrishnann6106@gmail.com>
2026-04-10 12:44:25 +0530

    Time: 0 ms (100%), Space: 43 MB (9.56%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        /*for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);*/
+        for (int money : nums) {
+            int curr = Math.max(rob,norob+money);
+            norob = rob;
+            rob = curr;
+        }
+        return rob;
+    }
+}
